230ffd076aea67b9bb04eb346079edb43403b34e
Sur <sur.lee1010@gmail.com>
2019-03-02 10:40:27 +0800

    add

--- demo.sh
+++ demo.sh
@@ -6,15 +6,9 @@ else
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
-  echo "<!DOCTYPE>
-        <title>Hello</title>
-        <h1>Hi</h1>"
-    > index.html
-  echo "h1{color: red;}"
-     > css/style.css
-  echo "var string = "Hello World"
-       alert(string)"
-     > js/main.js
+  echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+  echo -e "h1{color: red;}" > css/style.css
+  echo -e "var string = \"Hello World\"\nalert(string)" > js/main.js
   echo 'success'
   exit
 fi
