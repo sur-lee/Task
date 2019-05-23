791a16353c441f847235e8d9c3142ed01cc8bfc6
朱颜 <sur.lee1010@gmail.com>
2019-05-23 17:40:28 +0800

    modify

--- canvas/main.js
+++ canvas/main.js
@@ -1,42 +1,24 @@
 var yyy = document.getElementById('huabu');
 //设置尺寸
-setWindow()
-window.onresize = function(){
-    setWindow()
-}
-function setWindow(){
-    var pageWidth = document.documentElement.clientWidth
-    var pageHeight = document.documentElement.clientHeight
+autoSetCanvasSize(yyy)
 
-    yyy.width = pageWidth
-    yyy.height = pageHeight
-}
 var context = yyy.getContext('2d');
 
-function drawCircle(x,y,radius){
-    context.beginPath()
-    context.fillStyle = 'black'
-    context.arc(x,y,radius,0,Math.PI*2);
-    context.fill()
-}
-function drawLine(x1,y1,x2,y2){
-    context.beginPath();
-    context.fillStyle = 'black'
-    context.moveTo(x1,y1)
-    context.lineWidth = 6
-    context.lineTo(x2,y2)
-    context.stroke()
-    context.closePath()
-}
+
 var painting = false
 var lastPoint = {x: undefined, y: undefined}
 //鼠标按下
 yyy.onmousedown = function(aaa){
-    painting = true
     var x = aaa.clientX
     var y = aaa.clientY
-    lastPoint = {"x":x, "y":y}
-    drawCircle(x,y,3)
+    if(usingEraser){
+        context.clearRect(x,y,10,10)
+    } else {
+        painting = true
+        lastPoint = {"x":x, "y":y}
+        drawCircle(x,y,3)
+    }
+    
 }
 //移动鼠标
 yyy.onmousemove = function(aaa){
@@ -54,4 +36,40 @@ yyy.onmousemove = function(aaa){
 //鼠标抬起
 yyy.onmouseup = function(aaa){
     painting = false
+}
+
+
+/*****/
+function autoSetCanvasSize(canvas){
+    setWindow()
+        window.onresize = function(){
+            setWindow()
+        }
+    function setWindow(){
+        var pageWidth = document.documentElement.clientWidth
+        var pageHeight = document.documentElement.clientHeight
+
+        canvas.width = pageWidth
+        canvas.height = pageHeight
+    }
+}
+
+function drawCircle(x,y,radius){
+    context.beginPath()
+    context.fillStyle = 'black'
+    context.arc(x,y,radius,0,Math.PI*2);
+    context.fill()
+}
+function drawLine(x1,y1,x2,y2){
+    context.beginPath();
+    context.fillStyle = 'black'
+    context.moveTo(x1,y1)
+    context.lineWidth = 6
+    context.lineTo(x2,y2)
+    context.stroke()
+    context.closePath()
+}
+var usingEraser = false
+usingEraser.onclick = function(){
+    usingEraser = !usingEraser
 }
