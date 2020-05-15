7b1f8f09fb21b204fc1b44d0072958a6c735afe7
朱颜 <sur.lee1010@gmail.com>
2020-05-15 16:02:10 +0800

    苹果轮播

---
+++ appleSlideshow/main.js
@@ -0,0 +1,13 @@
+var allButtons = $('#menuItems > .menuItem')
+
+//点击轮播
+    for(let i = 0; i < allButtons.length; i++) {
+      $(allButtons[i]).on('click', function(x) {
+        var index = $(x.currentTarget).index() - 1
+        console.log(index)
+        var p = index * -920
+        $('#slides').css({
+          transform: 'translateX(' + p + 'px)'
+        })
+      })
+    }
