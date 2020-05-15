70948d13f1afa037402a39fa90c2cc88b86a00a9
朱颜 <sur.lee1010@gmail.com>
2020-05-15 16:25:23 +0800

    no message

--- appleSlideshow/main.js
+++ appleSlideshow/main.js
@@ -1,13 +1,19 @@
 var allButtons = $('#menuItems > .menuItem')
 
 //点击轮播
-    for(let i = 0; i < allButtons.length; i++) {
-      $(allButtons[i]).on('click', function(x) {
-        var index = $(x.currentTarget).index() - 1
-        console.log(index)
-        var p = index * -920
-        $('#slides').css({
-          transform: 'translateX(' + p + 'px)'
-        })
-      })
-    }
+for(let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on('click', function(x) {
+    var index = $(x.currentTarget).index() - 1
+    console.log(index)
+    var p = index * -920
+    $('#slides').css({
+      transform: 'translateX(' + p + 'px)'
+    })
+    n = index
+    activeButton(allButtons.eq(n))
+  })
+}
+
+function activeButton($button) {
+  $button.addClass('act').siblings('.act').removeClass('act')
+}
