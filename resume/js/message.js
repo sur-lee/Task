410329f2cb6e68da4c45f68852a560f29122e6f9
朱颜 <sur.lee1010@gmail.com>
2020-07-21 15:16:17 +0800

    message JS MVC 实现

--- resume/js/message.js
+++ resume/js/message.js
@@ -1,47 +1,87 @@
-AV.init({
-  appId: "LnRBf5MFi6aErBL3upu3yMJe-gzGzoHsz",
-  appKey: "x4fRswVJsOSXtnz82sqLPoLx",
-  serverURL: "https://lnrbf5mf.lc-cn-n1-shared.com"
-});
+! function () {
+  var view = document.querySelector('section.postMessage')
 
-const query = new AV.Query('Message');
-query.find()
-.then(
-  function (messages) {
-    let array = messages.map((item) => item.attributes)
-    array.forEach((item) => {
-      let li = document.createElement('li')
-      li.innerText = `${item.name}: ${item.content}`
-      let messageList = document.querySelector('#messageList')
-      messageList.appendChild(li)
-
-    })
+  var model = {
+    initAV: function () {
+      AV.init({
+        appId: "LnRBf5MFi6aErBL3upu3yMJe-gzGzoHsz",
+        appKey: "x4fRswVJsOSXtnz82sqLPoLx",
+        serverURL: "https://lnrbf5mf.lc-cn-n1-shared.com"
+      });
+    },
+    fetch: function () {
+      var query = new AV.Query('Message')
+      return query.find()
+    },
+    save: function (name, content) {
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      return message.save({
+        'name': name,
+        'content': content
+      })
+    }
   }
-)
+
+
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function (view, model) {
+      this.view = view
+      this.model = model
+
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.initAV()
+      this.loadmessages()
+      this.bindEvents()
+    },
+
+    loadmessages: function () {
+
+      this.model.fetch().then(
+        (messages) => {
+          let array = messages.map((item) => item.attributes)
+          array.forEach((item) => {
+            let li = document.createElement('li')
+            li.innerText = `${item.name}: ${item.content}`
+            this.messageList.appendChild(li)
+          })
+        }
+      )
+    },
+    bindEvents: function () {
+      this.form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        this.saveMessage()
+      })
+    },
+    saveMessage: function () {
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      this.model.save(name, content).then((object) => {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+        myForm.querySelector('input[name=name]').value = ''
+        console.log(object)
+      })
+    }
+  }
+
+  controller.init(view, model)
 
 
 
 
-let myForm = document.querySelector('#postMessageForm')
-
-myForm.addEventListener('submit', function(e){
-  e.preventDefault()
-  let content = myForm.querySelector('input[name=content]').value
-  let name = myForm.querySelector('input[name=name]').value
-  var Message = AV.Object.extend('Message');
-  var message = new Message();
-  message.save({
-    'name': name,
-    'content': content
-  }).then((object) => {
-    let li = document.createElement('li')
-    li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-    let messageList = document.querySelector('#messageList')
-    messageList.appendChild(li)
-    myForm.querySelector('input[name=content]').value = ''
-    myForm.querySelector('input[name=name]').value = ''
-    console.log(object)
-  })
-})
 
 
+
+
+
+}.call()
