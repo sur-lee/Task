016a9efa7c46e7fa44890450d394b98d763a4d61
朱颜 <sur.lee1010@gmail.com>
2020-07-21 14:04:14 +0800

    添加留言用户名

--- resume/js/message.js
+++ resume/js/message.js
@@ -11,7 +11,7 @@ query.find()
     let array = messages.map((item) => item.attributes)
     array.forEach((item) => {
       let li = document.createElement('li')
-      li.innerText = item.content
+      li.innerText = `${item.name}: ${item.content}`
       let messageList = document.querySelector('#messageList')
       messageList.appendChild(li)
 
@@ -27,12 +27,19 @@ let myForm = document.querySelector('#postMessageForm')
 myForm.addEventListener('submit', function(e){
   e.preventDefault()
   let content = myForm.querySelector('input[name=content]').value
+  let name = myForm.querySelector('input[name=name]').value
   var Message = AV.Object.extend('Message');
   var message = new Message();
   message.save({
+    'name': name,
     'content': content
   }).then((object) => {
-    window.location.reload()
+    let li = document.createElement('li')
+    li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+    let messageList = document.querySelector('#messageList')
+    messageList.appendChild(li)
+    myForm.querySelector('input[name=content]').value = ''
+    myForm.querySelector('input[name=name]').value = ''
     console.log(object)
   })
 })
